e1a55bcf4d5fead48c56b28be8abee5d920f1f08
BrukAbebe <birukgalk@gmail.com>
2024-02-09 00:09:13 +0300

    First Header and Footer

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./home.css";
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+function Home() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+}
+
+export default Home
